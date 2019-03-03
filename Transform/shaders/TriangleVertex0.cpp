669928010e2fa469b198db0d6d1c89548cbd87b2
yx duan <mjdyx@outlook.com>
2019-03-03 10:50:44 +0800

    Add Transform

---
+++ Transform/shaders/TriangleVertex0.cpp
@@ -0,0 +1,13 @@
+#version 450 core
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 color;
+layout(std140, row_major, binding = 0)uniform transBuffer
+{
+	mat4 trans;
+};
+out vec4 in_color;
+void main()
+{
+	gl_Position = trans * vec4(position, 1);
+	in_color = vec4(color, 1);
+}
