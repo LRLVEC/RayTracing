c8d080b5a18cb102ed4845474280500fc1a5070a
yx duan <mjdyx@outlook.com>
2019-04-26 23:01:50 +0800

    a

--- BVH/BVH.cpp
+++ BVH/BVH.cpp
@@ -242,7 +242,7 @@ namespace OpenGL
 			decayOriginStorage(&decayOriginBuffer, ShaderStorageBuffer, 8),
 			testBMP("resources\\Haja1.bmp"),
 			cubeData("resources\\room\\"),
-			//stl(sm.folder.find("resources/compound_j74_smp.stl").readSTL()),
+			stl(sm.folder.find("resources/compound_j74_smp.stl").readSTL()),
 			texture(&testBMP, 1),
 			cube(&cubeData, 2, RGBA32f, 1, cubeData.bmp[0].header.width, cubeData.bmp[0].header.height),
 			textureConfig(&texture, Texture2DArray, RGBA32f, 1, testBMP.bmp.header.width, testBMP.bmp.header.height, 1),
@@ -257,20 +257,34 @@ namespace OpenGL
 			texture.bindUnit();
 			cube.bindUnit();
 			glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
-			
-			//model.addSTL
-			//(
-			//	stl,
-			//	{
-			//		1,-1,
-			//		0,-1,
-			//		0,-1,
-			//		0,-1,
-			//		0,//{ -0.03,0,-0.03 },
-			//		1
-			//	},
-			//	stl.triangles.length
-			//);
+
+			/*model.circles.data.circles +=
+			{
+				{
+
+				}
+			};*/
+
+			model.pointLights.data.pointLights +=
+			{
+				{
+					{4000, 4000, 4000},
+					{ 0,-300,0 }
+				}
+			};
+			model.addSTL
+			(
+				stl,
+				{
+					1,-1,
+					0,-1,
+					0,-1,
+					0,-1,
+					0,//{ -0.03,0,-0.03 },
+					1
+				},
+				stl.triangles.length
+			);
 			model.planes.numChanged = true;
 			model.triangles.numChanged = true;
 			model.spheres.numChanged = true;
